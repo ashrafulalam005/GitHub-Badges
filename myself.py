24ce987db9a4f0176e12b15560e0afbb4af843be
Ashraful Alam <ashrafulalamyt@gmail.com>
2024-09-03 09:07:57 +0600

    Create myself.py

---
+++ myself.py
@@ -0,0 +1,4 @@
+print("Hello! My name is Ashraful Alam.")
+print("I am from Chittagong, Bangladesh.")
+print("I am studying B.Sc. in Computer Science and Engineering at International Islamic University Chittagong (IIUC).")
+print("I am a passionate software developer with interests in solving algorithmic challenges, developing web and mobile applications, and competitive programming.")
